56d86c9151f366fb2147bcf5721c01bbe58f7cd5
rostom44 <blackgamerdv7@gmail.com>
2024-07-17 09:48:15 +0200

    done

---
+++ server/database/models/UserRepository.js
@@ -0,0 +1,69 @@
+const AbstractRepository = require("./AbstractRepository");
+
+class UserRepository extends AbstractRepository {
+  constructor() {
+    // Call the constructor of the parent class (AbstractRepository)
+    // and pass the table name "user" as configuration
+    super({ table: "user" });
+  }
+
+  // The C of CRUD - Create operation
+
+  async create(user) {
+    if (
+      !user ||
+      !user.firstname ||
+      !user.lastname ||
+      !user.email ||
+      !user.password
+    ) {
+      throw new Error("Missing required user fields");
+    }
+
+    // Execute the SQL INSERT query to add a new user to the "user" table
+    const [result] = await this.database.query(
+      `insert into ${this.table} (firstname, lastname, email, password) values (?, ?, ?, ?)`,
+      [user.firstname, user.lastname, user.email, user.password]
+    );
+
+    // Return the ID of the newly inserted user
+    return result.insertId;
+  }
+
+  // The Rs of CRUD - Read operations
+
+  async read(id) {
+    // Execute the SQL SELECT query to retrieve a specific user by its ID
+    const [rows] = await this.database.query(
+      `select * from ${this.table} where id = ?`,
+      [id]
+    );
+
+    // Return the first row of the result, which represents the user
+    return rows[0];
+  }
+
+  async readAll() {
+    // Execute the SQL SELECT query to retrieve all users from the "user" table
+    const [rows] = await this.database.query(`select * from ${this.table}`);
+
+    // Return the array of users
+    return rows;
+  }
+
+  // The U of CRUD - Update operation
+  // TODO: Implement the update operation to modify an existing user
+
+  // async update(user) {
+  //   ...
+  // }
+
+  // The D of CRUD - Delete operation
+  // TODO: Implement the delete operation to remove an user by its ID
+
+  // async delete(id) {
+  //   ...
+  // }
+}
+
+module.exports = UserRepository;
--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,12 +1,8 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
-);
