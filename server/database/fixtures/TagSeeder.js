4070150f291a1086b3ca4a08d6014690737e5418
rostom44 <blackgamerdv7@gmail.com>
2024-07-19 01:23:43 +0200

    still_the_cookie_is_not_working

--- server/database/fixtures/TagSeeder.js
+++ server/database/fixtures/TagSeeder.js
@@ -7,15 +7,30 @@ class TagSeeder extends AbstractSeeder {
 
   run() {
     // Generate and insert fake data into the 'tag' table
-    for (let i = 0; i < 10; i += 1) {
-      // Adjust the number of tags as needed
-      const fakeTag = {
-        name: this.faker.word.adjective(), // Generate a fake tag name
-      };
+    const fakeTags = [
+      { id: 1, name: "car" },
+      { id: 2, name: "turbo" },
+      { id: 3, name: "Audi" },
+      { id: 4, name: "engine" },
+      { id: 5, name: "drift" },
+      { id: 6, name: "sport" },
+      { id: 7, name: "electric" },
+      { id: 8, name: "supercar" },
+      { id: 9, name: "sport car" },
+      { id: 10, name: "tips" },
+      { id: 11, name: "tuning" },
+      { id: 12, name: "performance" },
+      { id: 13, name: "luxury" },
+      { id: 14, name: "safety" },
+      { id: 15, name: "suspension" },
+      { id: 16, name: "wheels" },
+      { id: 17, name: "roadster" },
+    ];
 
-      // Insert the fakeTag data into the 'tag' table
+    // Insert each tag into the 'tag' table
+    fakeTags.forEach((fakeTag) => {
       this.insert(fakeTag);
-    }
+    });
   }
 }
 
