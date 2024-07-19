699e5c9512a98f103947fc12de99e7ca37c8ad21
rostom44 <blackgamerdv7@gmail.com>
2024-07-19 11:37:42 +0200

    fixing_stuff

--- client/src/context/userContext.jsx
+++ client/src/context/userContext.jsx
@@ -4,6 +4,13 @@ import UseLocalStorage from "../UselocalStorage";
 
 const UserContext = createContext();
 
+/**
+ * The useMemo hook is used to memoize the value returned by the callback function.
+ * It will only recompute the memoized value when one of the dependencies has changed.
+ * In this case, the memoized value is an object containing the user data, the setUser function, and the login function.
+ * The user data is stored in local storage and context, and the setUser function is used to update the user data.
+ * The login function is used to log the user in by storing their data in local storage and context.
+ */
 export function UserProvider({ children }) {
   const [user, setUser] = UseLocalStorage("user", null);
 
